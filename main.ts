5fad77766302733bcbdf0346b20db243d55491da
Vakiyama <vakiyama@outlook.com>
2023-09-14 22:30:12 -0700

    added spaces to question calls

--- main.ts
+++ main.ts
@@ -6,9 +6,9 @@ const rl = readline.createInterface({ input, output });
 makeCalendar(2022);
 
 async function getDayOfTheWeekForUserDate() {
-    const year = await rl.question("Please input a year (e. g. 1999):");
-    const month = await rl.question("Please input a month (e. g. August):");
-    const day = await rl.question("Please input a day (e. g. 23):");
+    const year = await rl.question("Please input a year (e. g. 1999): ");
+    const month = await rl.question("Please input a month (e. g. August): ");
+    const day = await rl.question("Please input a day (e. g. 23): ");
     const dayOfTheWeek = getDayOfTheWeek(parseInt(year), month, parseInt(day));
 
     console.log(`Your day of the week is: ${dayOfTheWeek}`);
